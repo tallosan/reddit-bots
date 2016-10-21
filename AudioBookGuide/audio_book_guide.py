c218c992b8502cd4d63d456afb01aef3818c8015
tallosan <andrew.tallos@mail.utoronto.ca>
2016-10-21 18:21:11 -0400

    Added: New AudioBookGuide Bot

---
+++ AudioBookGuide/audio_book_guide.py
@@ -0,0 +1,152 @@
+import praw
+from time import sleep
+import requests
+
+
+USER_AGENT="audioBookGuide v1.0"
+TARGET_SUB="audiobooksonyoutube"
+
+GOODREADS_KEY=""
+GOODREADS_SECRET=""
+
+YOUTUBE_KEY=""
+
+
+'''	Parse the newest submissions in the target sub. Return each
+	submissions' title, and the Youtube ID of the linked audiobook. '''
+def parse_submissions(target_sub):
+	
+	r = praw.Reddit(user_agent=USER_AGENT)
+	subreddit = r.get_subreddit(TARGET_SUB)
+
+	# Create an array of titles (where possible), and their respective
+	# video ID.
+	titles, ids = [], []
+	for submission in subreddit.get_new():
+		title = parse_title(submission.title)
+		if title:
+			titles.append(title)
+			ids.append(get_video_id(submission.url))
+
+	return titles, ids
+
+
+'''	Parse the 'title', assuming the correct format is used. Toggle
+	'verbose' for error messages if desired. '''
+def parse_title(title, verbose=False):
+
+	# Attempt to parse the title. If it isn't formatted correcly then
+	# simply return None.
+	try:
+		term = title.index('(')
+		return title[:term]
+	except ValueError as val_error:
+		if verbose: print "error: " + str(val_error)
+
+
+'''	Return the ID of a Youtube video, from a Youtube URL. '''
+def get_video_id(url):
+	
+	start = url.find('v=') + 2
+	end = url.find('&')
+	
+	# Handle 'youtu.be' links.
+	if '.be' in url:
+		start = url.find('.be/') + 4
+		return url[start:]
+	if '?list=' in url:
+		print 'PLAYLIST'
+		start = url.find('?list=') + 6
+		print url[start:]
+		return url[start:]
+	
+	return url[start:end]
+
+
+'''	Create valid query URLs for the Goodreads API. '''
+def linkify(titles):
+	
+	gr_base	= "https://www.goodreads.com/book/title.xml?title="
+	gr_suffix = "&key=" + GOODREADS_KEY
+	
+	urls = []
+	for title in titles:
+		url = gr_base + title + gr_suffix
+		urls.append(url)
+
+	return urls
+
+
+'''	Return an array containing the desired metadata from the given
+	Goodreads formatted URL. '''
+def get_book_data(url):
+
+	# Read in the XML from the given URL.
+	import xmltodict
+	
+	r = requests.get(url)
+	xml = xmltodict.parse(r.text)
+
+	# Gather Book metadata.
+	book_data 	= xml['GoodreadsResponse']['book']
+	title 		= book_data['title']
+	author		= book_data['authors']['author']['name']
+	date		= book_data['publication_year'], \
+			  	book_data['publication_month'], \
+			  	book_data['publication_day']
+	description	= book_data['description']
+	rating		= book_data['average_rating']
+
+	print title
+	print author
+	print description
+	print date, rating
+
+
+'''	Create valid query URLS for the Youtube API, using video IDs. '''
+def linkify_youtube(video_ids):
+
+	youtube_base 	= "https://www.googleapis.com/youtube/v3/videos?id="
+	youtube_suffix 	= "&part=contentDetails&key=" + YOUTUBE_KEY
+	
+	urls = []
+	for video_id in video_ids:
+		yt_url = youtube_base + video_id + youtube_suffix
+		urls.append(yt_url)
+
+	return urls
+
+
+'''	Get metadata from the given Youtube URL. '''
+def get_audio_data(url):
+	
+	import json
+	
+	r = requests.get(url)
+	json_data = json.loads(r.text)
+	
+	#TODO: Use the Youtube API to handle playlist requests. Returning
+	# 'Playlist' for now suffices, but is hardly very useful.
+	try:	
+		iso_duration = json_data['items'][0] \
+					['contentDetails'] \
+					['duration']
+	except IndexError:
+		return "Playlist."
+	
+	'''	(Helper) Youtube returns video durations in ISO-8601 format.
+		We can convert this using the 'isodate' library. '''
+	def convert_iso(duration):
+		import isodate
+		return isodate.parse_duration(iso_duration)
+
+	return convert_iso(iso_duration)
+
+
+titles, ids 	= parse_submissions(TARGET_SUB)
+print ids[5]
+gr_links 	= linkify(titles)
+yt_links	= linkify_youtube(ids)
+book		= get_book_data(gr_links[5])
+print get_audio_data(yt_links[5])
+
