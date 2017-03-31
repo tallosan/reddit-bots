0a160b355c67d6f46a21ed3e1a84e2a41b63c893
tallosan <andrew.tallos@mail.utoronto.ca>
2017-03-31 18:20:20 -0400

    Update: Finished Webcrawler, and started on thread formatting & posting.

--- RugbyBot/rugby_bot.py
+++ RugbyBot/rugby_bot.py
@@ -9,6 +9,54 @@ import requests
 from lxml import html
 
 
+'''   Responsible for getting any rugby matches scheduled, and creating threads
+      when necessary. '''
+class Scheduler(object):
+
+	''' Create a scheduler.
+	    Args:
+	    	poll_interval: The interval between polls.
+		*url: The URL to get the match data from.
+	'''
+	def __init__(self, url='http://www.espn.co.uk'):
+		
+		self.base_url 	   = url
+		self.url 	   = self.base_url + '/rugby/scoreboard'
+		self.matches	   = []
+	
+	''' Run the scheduler to determine which match threads need to be created.
+	    Args:
+		poll_interval: The interval between polls.
+	'''
+	def run_scheduler(self, poll_interval):
+		
+		matches = self.get_matches(self.url)
+		for match in matches:
+			if match.is_posted:
+				match.update_thread()
+			else:
+				match.post_thread()
+			laksjd;
+
+	''' Returns a list of Match objects that are not currently in the
+	    scheduler's cache.
+	'''
+	def get_matches(self, url):
+
+		request = requests.get(url)
+		tree = html.fromstring(request.content)
+
+		# Get the relative match URLs, and create full URLs.
+		schedule = tree.xpath('//a[@class="competitors"]')
+		matches  = []
+		for match in schedule:
+			matches.append(
+					Match(self.base_url + match.get('href'))
+			)
+		
+		return matches
+
+
 '''   Represents a rugby union match. '''
 class Match(object):
 
@@ -17,14 +65,67 @@ class Match(object):
 
                 self.url = url
 
-                self.competition = None
+                # Match data.
+		self.competition = None
+                self.game_time   = None
+		self.key_events	 = None
+		self.thread 	 = ""
 
                 self.home_team   = {}
                 self.away_team   = {}
 
-                self.game_time   = None
+		# Monitors the status of the game.
+		self.is_posted	 = False
+		self.is_active	 = False
 
+		# Populate fields.
                 self.parse_gamethread(self.url)
+ 
+ 	''' Create a thread for this Match. '''
+	def post_thread(self):
+                
+		# Score
+		# Try scorers
+		# Thread title.
+		title = "# Match Thread: " + \
+			self.home_team['name'] + ' ' + self.game_time + \
+			self.away_team['name'] + '[' + self.competition + ']'
+		
+		# Title / Header.
+		thread = ""
+		thread += '# ' + self.home_team['name'] + ' vs ' + self.away_team['name']
+		thread += '\n\n----\n\n'
+		
+		# Lineups: Starters, then Replacements.
+		thread += "## Lineups:\n\n"
+		thread += "|:--:|\n"
+		thread += "**" + self.home_team['name'] + "** | **" + \
+				 self.away_team['name'] + "**\n"
+		
+		thread += "**Player** | **Position** | **Player** | **Position**\n\n"
+		for h_player, a_player in zip(self.home_team['starters'],
+					      self.away_team['starters']):
+			thread += h_player[0] + '  ' + h_player[1] + ' | ' + h_player[2]
+			thread += ' | '
+			thread += a_player[0] + '  ' + a_player[1] + ' | ' + a_player[2]
+			thread += '\n'
+		thread += "|:--:|\n"
+		thread += "| **Replacements** |\n"
+		thread += "**Player** | **Position** | **Player** | **Position**\n\n"
+		for h_sub, a_sub in zip(self.home_team['subs'], self.away_team['subs']):
+			thread += h_sub[0] + '  ' + h_sub[1] + ' | ' + h_sub[2]
+			thread += ' | '
+			thread += a_sub[0] + '  ' + a_sub[1] + ' | ' + a_sub[2]
+			thread += '\n'
+		
+		thread += "----"
+		self.thread = thread
+		
+		# Set posted flag.
+		self.is_posted = True
+	
+	def update_thread(self):
+		pass
 
         ''' Parse the website linked via the 'url' parameter, and return all
             relevant match info (e.g. score, current time in game, etc).
@@ -36,7 +137,6 @@ class Match(object):
                 request = requests.get(url)
                 tree = html.fromstring(request.content)
 
-                #TODO: Don't forget to use this!
                 # Get the competition name.
                 competition = tree.xpath('//*[@id="custom-nav"]/header/div[1]')[0]
                 self.competition = competition.text_content()
@@ -57,7 +157,6 @@ class Match(object):
                 self.away_team['name']  = a_team.text_content()
                 self.away_team['score'] = a_score.text_content()
 
-                #TODO: Don't forget to use this!
                 # Get the current game time.
                 time = tree.xpath('//*[@id="custom-nav"]/header/div[2]/div[2]/span[3]')
                 self.game_time = time[0].text_content()
@@ -85,20 +184,12 @@ class Match(object):
                 self.home_team['subs'] 	   = self.get_lineup(h_subs)
 		self.away_team['starters'] = self.get_lineup(a_lineup)
 		self.away_team['subs']	   = self.get_lineup(a_subs)
-                print self.home_team['starters']
-                print
-                print self.home_team['subs']
-		print
-  		print self.away_team['starters']
-                print
-  		print self.away_team['subs']
-                print
 
-        def create_thread(self):
-                pass
+		# TODO: Get KeyEvents.
+		events = tree.xpath('//*[@id="tab1"]/table/tbody')[0]
+		self.key_events = self.get_key_events(events)
 
-
-        ''' Parses the 'tries' div, and returns a list of tuples where the first
+	''' Parses the 'tries' div, and returns a list of tuples where the first
             element is the try scorer's name, and the second is the try time.
             Args:
                 e_tries: An HtmlElement containing the match's try data.
@@ -128,45 +219,50 @@ class Match(object):
 			# ESPN occasionally screws up the last row in the div, so
 			# we need to handle this.
 			try:
-				row = e.getchildren()[0].\
-				        getchildren()[0].\
-					getchildren()[0]
+				row = e.getchildren()[0].getchildren()[0].getchildren()[0]
 				
 				# Parse the player's number, then their name and position.
 				number = row.getchildren()[1].text_content()
 				player = row.getchildren()[2].text_content().split(',')
 				name   = player[0]
 				pos    = player[1][1:].split(' ')[0]
-
-				players.append( (number, name, pos) )
 			
 			except IndexError:
-				pass
+				number = row.getchildren()[0].text_content()
+
+				player = row.getchildren()[1].text_content().split(',')
+				name   = player[0]
+				pos    = player[1].strip()
+			
+			finally:
+				players.append( (number, name, pos) )
 
                 return players
 
-
-def run_scheduler():
-        pass
+	def get_key_events(self, event_element):
+		pass
 
 
 URL = 'http://www.espn.co.uk/rugby/match?gameId=290096&league=267979'
-URL = 'http://www.espn.co.uk/rugby/match?gameId=290097&league=267979'
-m = Match(URL)
+
+scheduler = Scheduler()
+scheduler.run_scheduler(poll_interval=30)
 
 laskdj;
-
-
 # ========================================================================
 
-USER_AGENT = 'RugbyUBot'
-USERNAME   = 'RugbyUBot'
-PASSWORD   = None
+USER_AGENT = 'RugbyUnionBot'
+USERNAME   = 'RugbyUnionBot'
+PASSWORD   = 'rugbyunionbot1234'
+
+POLL_INTERVAL = 30
 
 r = praw.Reddit(user_agent=USER_AGENT)
 r.login(username=USERNAME, password=PASSWORD, disable_warning=True)
 
 
 if __name__=='__main__':
-        run_scheduler()
+        
+	scheduler = Scheduler()
+	scheduler.run_scheduler(poll_interval=POLL_INTERVAL)
 
